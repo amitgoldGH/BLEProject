aa8817c8db6de32f3e6b82185bcfc248e748829b
pristit <epicnesssauce@gmail.com>
2022-05-06 01:22:19 +0300

    Created file for all the constants in the app

---
+++ constants/constants.js
@@ -0,0 +1,35 @@
+// BACKEND
+export const SPRING_SERVER_ADDRESS = "http://10.100.102.18:8091";
+export const SAMPLE_CREATE = "/api/samples";
+export const SESSION_CREATE = "/api/sessions";
+export const SESSION_GET = "/api/sessions";
+export const USER_CREATE = "/api/users";
+export const USER_LOGIN = "/api/users/login";
+
+// MANIKIN
+export const DEVICE_ADVERTISED_NAME = "Manikin";
+export const DEVICE_MAC_ADDRESS = "18:93:D7:07:D7:1E";
+export const PERIPHERAL_SERVICE_UUID = "0000ffe0-0000-1000-8000-00805f9b34fb";
+export const PERIPHERAL_CHARACTERISTIC_UUID =
+  "0000ffe1-0000-1000-8000-00805f9b34fb";
+
+// PAIRING
+export const TIMEOUT_COUNT = 20;
+export const SLEEP_TIME_MS = 1000;
+export const NUMBER_OF_DEVICES_TO_SCAN = 10;
+
+// BVM
+export const PRESSURE_TOP = 1.7;
+export const PRESSURE_BOT = 0.9;
+export const PRESSURE_SAMPLE_COUNT = 10;
+
+// CPR
+export const GOOD_RATE_TOP = 100;
+export const GOOD_RATE_BOTTOM = 50;
+export const GOOD_DEPTH_TOP = 6;
+export const GOOD_DEPTH_BOT = 4;
+
+// SHAPES
+export const CIRCLE_DIM = 35;
+export const CIRCLE_BORDER_RADIUS = 1000;
+export const CIRCLE_BORDER_WIDTH = 2;
