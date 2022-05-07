dd5cf086d4d2ab9898774bc11784aa8fe76cf92a
pristit <epicnesssauce@gmail.com>
2022-05-07 21:20:12 +0300

    Changed app.js to use constants

--- App.js
+++ App.js
@@ -23,10 +23,25 @@ import RegistrationPage from "./Screens/RegistrationPage";
 import LandingPage from "./Screens/LandingPage";
 import PairingPage from "./Screens/PairingPage";
 import SettingsPage from "./Screens/SettingsPage";
-// import MeasurementPage from "./Screens/MeasurementPage";
 import BVMMeasurementPage from "./Screens/BVMMeasurementPage";
 import CPRMeasurementPage from "./Screens/CPRMeasurementPage";
+import ExerciseResultPage from "./Screens/ExerciseResultPage";
+
+import {
+  LOGIN_PAGE_TITLE,
+  REGISTRATION_PAGE_TITLE,
+  SETTING_PAGE_TITLE,
+  LANDING_PAGE_TITLE,
+  PAIRING_PAGE_TITLE,
+  RESULT_PAGE_TITLE,
+  BVM_MEASUREMENT_PAGE_TITLE,
+  CPR_MEASUREMENT_PAGE_TITLE,
+} from "./constants/constants";
 import { BleManager } from "react-native-ble-plx";
+
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["new NativeEventEmitter"]); // Ignore log notification by message
+
 // import { render } from "react-native/Libraries/Renderer/implementations/ReactNativeRenderer-dev";
 
 const DeviceManager = new BleManager();
@@ -43,14 +58,27 @@ const App: () => Node = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Login" component={LoginPage} />
-          <Stack.Screen name="Registration" component={RegistrationPage} />
-          <Stack.Screen name="Home" component={LandingPage} />
-          <Stack.Screen name="Settings" component={SettingsPage} />
-          <Stack.Screen name="Pairing" component={PairingPage} />
-          <Stack.Screen name="CPR_Measurement" component={CPRMeasurementPage} />
-          <Stack.Screen name="BVM_Measurement" component={BVMMeasurementPage} />
+        <Stack.Navigator initialRouteName={LOGIN_PAGE_TITLE}>
+          <Stack.Screen name={LOGIN_PAGE_TITLE} component={LoginPage} />
+          <Stack.Screen
+            name={REGISTRATION_PAGE_TITLE}
+            component={RegistrationPage}
+          />
+          <Stack.Screen name={LANDING_PAGE_TITLE} component={LandingPage} />
+          <Stack.Screen name={SETTING_PAGE_TITLE} component={SettingsPage} />
+          <Stack.Screen name={PAIRING_PAGE_TITLE} component={PairingPage} />
+          <Stack.Screen
+            name={CPR_MEASUREMENT_PAGE_TITLE}
+            component={CPRMeasurementPage}
+          />
+          <Stack.Screen
+            name={BVM_MEASUREMENT_PAGE_TITLE}
+            component={BVMMeasurementPage}
+          />
+          <Stack.Screen
+            name={RESULT_PAGE_TITLE}
+            component={ExerciseResultPage}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
