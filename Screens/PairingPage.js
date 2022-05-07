519cdf67a21961b639461b3c77621d66c8f2d3f0
pristit <epicnesssauce@gmail.com>
2022-05-07 21:15:08 +0300

    removed redundant buttons  and added constants

--- Screens/PairingPage.js
+++ Screens/PairingPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
   ImageBackground,
   StyleSheet,
@@ -6,30 +6,17 @@ import {
   View,
   Text,
   PermissionsAndroid,
-  FlatList,
 } from "react-native";
 
-import {
-  BleManager,
-  Characteristic,
-  Device,
-  Service,
-} from "react-native-ble-plx";
-import { Buffer } from "buffer";
-import { useStore, connect } from "react-redux";
+import { useStore } from "react-redux";
+
+import { connectDevice, setUsername, startScan } from "../actions";
 
 import {
-  connectDevice,
-  startScan,
-  changeMode,
-  changeStringValue,
-} from "../actions";
-
-import {
+  CPR_MEASUREMENT_PAGE_TITLE,
+  BVM_MEASUREMENT_PAGE_TITLE,
   DEVICE_ADVERTISED_NAME,
   DEVICE_MAC_ADDRESS,
-  PERIPHERAL_SERVICE_UUID,
-  PERIPHERAL_CHARACTERISTIC_UUID,
   TIMEOUT_COUNT,
   SLEEP_TIME_MS,
 } from "../constants/constants";
@@ -54,13 +41,15 @@ const sleep = (milliseconds) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
-const deviceList = [];
-
-function PairingPage({ navigation }) {
+function PairingPage({ navigation, route }) {
   const store = useStore();
   const [manikinDetectionState, setManikinDetectionState] =
     React.useState(false);
 
+  React.useEffect(() => {
+    if (route.params.username != null)
+      store.dispatch(setUsername(route.params.username));
+  }, [route.params.username, store]);
   const [connectState, setConnectState] = React.useState(false);
 
   const [statusTextState, setStatusTextState] = React.useState("Idle");
@@ -156,167 +145,6 @@ function PairingPage({ navigation }) {
     setStatusTextState("Connected!");
   };
 
-  // const testFunction = () => {
-  //   const currState = store.getState();
-  //   currState.BLEs.BLEList.forEach((device) => {
-  //     console.log("in for each, device: ", device.name);
-  //     if (!DATA.some((item) => item.id === device.id))
-  //       DATA.push({ name: device.name, id: device.id });
-  //   });
-  //   console.log(DATA);
-  // };
-
-  const discoverOnPress = async () => {
-    checkLocationPermission().then((res) => {
-      console.log("Location permission ", res);
-      if (!res) {
-        requestLocationPermission();
-      }
-    });
-
-    let currStoreState = store.getState();
-    let wantedDevice;
-    currStoreState.BLEs.BLEList.forEach((device) => {
-      console.log("in for each, device: ", device.name);
-      if (
-        device.id === DEVICE_MAC_ADDRESS ||
-        device.name === DEVICE_ADVERTISED_NAME
-      ) {
-        setManikinDetectionState(true);
-        wantedDevice = store.dispatch(connectDevice(device));
-      }
-    });
-    console.log("Exited for each");
-    console.log("In async function before while loop");
-    while (
-      currStoreState.BLEs.status != "Listening" &&
-      currStoreState.BLEs.status != "Connected"
-    ) {
-      currStoreState = store.getState();
-      console.log(
-        "Waiting in async function for device to discover, status: ",
-        currStoreState.BLEs.status
-      );
-
-      await sleep(SLEEP_TIME_MS);
-    }
-    setConnectState(true);
-    // monitorInput(currStoreState.BLEs.connectedDevice);
-  };
-
-  // setCurrentState({ subscription: charSubscription, isSubscribed: true });
-
-  // testFunction();
-  //console.log("requested perms");
-
-  //console.log(checkLocationPermission());
-  // if (manager.state() === 'PoweredOn') {
-  //     scanAndConnect();
-  // }
-
-  // console.log("bluetooth state: " + currentBluetoothState);
-  // console.log("discover press");
-  // scanAndConnect();
-
-  const testSendCPRButton = () => {
-    console.log("Send CPR test button, connect state: " + connectState);
-    if (connectState == true) {
-      // Sends "TEST TEXT" in base64
-      // deviceList[0].writeCharacteristicWithResponseForService('FFE0', 'FFE1', 'VEVTVCBURVhU');
-      deviceList[0].writeCharacteristicWithResponseForService(
-        "FFE0",
-        "FFE1",
-        Buffer.from("0", "ascii").toString("base64")
-      );
-    }
-  };
-  const testSendBVMButton = () => {
-    console.log("Send BVM test button, connect state: " + connectState);
-    if (connectState == true) {
-      // Sends "TEST TEXT" in base64
-      // deviceList[0].writeCharacteristicWithResponseForService('FFE0', 'FFE1', 'VEVTVCBURVhU');
-      deviceList[0].writeCharacteristicWithResponseForService(
-        "FFE0",
-        "FFE1",
-        Buffer.from("1", "ascii").toString("base64")
-      );
-    }
-  };
-  const testSendStopModeButton = () => {
-    console.log("Send Stop test button, connect state: " + connectState);
-    if (connectState == true) {
-      // Sends "TEST TEXT" in base64
-      // deviceList[0].writeCharacteristicWithResponseForService('FFE0', 'FFE1', 'VEVTVCBURVhU');
-      deviceList[0].writeCharacteristicWithResponseForService(
-        "FFE0",
-        "FFE1",
-        Buffer.from("2", "ascii").toString("base64")
-      );
-    }
-  };
-
-  const testReadButton = async () => {
-    console.log("Read test button, connect state: ", connectState);
-    if (connectState == true && deviceList[0] != null) {
-      // if(!currentState.isSubscribed) {
-      //     const charSubscription = deviceList[0].monitorCharacteristicForService(PERIPHERAL_SERVICE_UUID, PERIPHERAL_CHARACTERISTIC_UUID, (error, char) => {
-      //                 if (error != null)
-      //                     console.log(error);
-      //                 console.log("New read!");
-      //                 setCurrentState({value: Buffer.from(char.value, 'base64').toString('ascii')})
-      //                 // console.log(Buffer.from(char.value, 'base64').toString('ascii'));
-      //             });
-      //             setCurrentState({subscription : charSubscription, isSubscribed : true})
-      // }
-      // deviceList[0].readCharacteristicForService('FFE0', 'FFE1').then((characteristics) => {
-      //     console.log(characteristics);
-      // })
-      // const discoveredDevice = await deviceList[0].discoverAllServicesAndCharacteristics();
-      // const services = await discoveredDevice.services();
-      // services.forEach(async service =>
-      //     {
-      //     const characteristics = await discoveredDevice.characteristicsForService(service.uuid);
-      //     characteristics.forEach(characteristic => {
-      //         if (characteristic.uuid.startsWith('0000FFE1')) {
-      //             console.log(characteristic);
-      //         }})
-      //     });
-    }
-  };
-
-  const testDisconnectButton = () => {
-    console.log("store state ", store.getState());
-    // console.log("Disconnect button, connect state: " + connectState);
-    // console.log("Disconnect button devicelist[0]: ", deviceList[0].name);
-    // // if (connectState == true)
-
-    // if (deviceList[0] != null) {
-    //   if (currentState.isSubscribed) currentState.subscription.remove();
-    //   console.log("Disconnect button myDevice != null");
-    //   deviceList[0].cancelConnection();
-    //   setConnectState(false);
-    //   setCurrentState({
-    //     ...currentState,
-    //     isSubscribed: false,
-    //     subscription: null,
-    //   });
-    // }
-  };
-  const testButton = () => {
-    // console.log(store.getState());
-    // store.dispatch(changeMode("1"));
-
-    store.dispatch(startScan());
-    // let currStore = store.getState();
-    // currStore.BLEs.BLEList.forEach((device) => {
-    //   console.log("in for each, device: ", device.name);
-    //   if (
-    //     device.id === DEVICE_MAC_ADDRESS ||
-    //     device.name === DEVICE_ADVERTISED_NAME
-    //   )
-    //     setManikinDetectionState(true);
-  };
-
   return (
     <ImageBackground
       style={styles.background}
@@ -342,46 +170,27 @@ function PairingPage({ navigation }) {
           <TouchableOpacity style={styles.scanButton} onPress={scanForDevices}>
             <Text style={styles.scanButtonText}>Scan and Connect</Text>
           </TouchableOpacity>
-          {/* <TouchableOpacity
-            style={styles.discoverButton}
-            onPress={discoverOnPress}
-          >
-            <Text>Discover nearby devices button</Text>
-          </TouchableOpacity> */}
-          {/* <TouchableOpacity
-            style={styles.discoverButton}
-            onPress={testReadButton}
-          >
-            <Text>test read</Text>
-          </TouchableOpacity> */}
-          {/* <TouchableOpacity
-            style={styles.discoverButton}
-            onPress={testDisconnectButton}
-          >
-            <Text>test disconnect</Text>
-          </TouchableOpacity> */}
-          {/* <TouchableOpacity style={styles.discoverButton} onPress={testButton}>
-            <Text>test button</Text>
-          </TouchableOpacity> */}
-          {/* <View style={styles.selectLabel}>
-            <Text>Select the manikin device and pair</Text>
-            <Text>Value:</Text>
-          </View> */}
-
-          {/* <View style={styles.discoveredDevicesBox}>
-                        <Text>placeholder devices</Text>
-                    </View> */}
         </View>
         <View style={styles.bottomSegment}>
           <TouchableOpacity
             style={styles.pairButton}
-            onPress={() => navigation.navigate("CPR_Measurement")}
+            onPress={() =>
+              navigation.navigate(CPR_MEASUREMENT_PAGE_TITLE, {
+                username: route.params.username,
+                type: "CPR",
+              })
+            }
           >
             <Text style={styles.buttonText}>CPR</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.pairButton}
-            onPress={() => navigation.navigate("BVM_Measurement")}
+            onPress={() =>
+              navigation.navigate(BVM_MEASUREMENT_PAGE_TITLE, {
+                username: route.params.username,
+                type: "BVM",
+              })
+            }
           >
             <Text style={styles.buttonText}>BVM</Text>
           </TouchableOpacity>
